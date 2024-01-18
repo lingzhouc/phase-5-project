d366127f75bb01c47a138ecdaad44f2afc5f12fa
LingZhouChen <lingzhouc@gmail.com>
2024-01-18 02:24:36 -0500

    ADD: started homepage && general tweaks

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,10 +1,26 @@
+import { Link, useNavigate} from 'react-router-dom';
+import confetti from 'canvas-confetti';
 import "../styling/homepage.css"
 
 function HomePage () {
+    const navigate = useNavigate();
+
+    const handleClick = () => { 
+        confetti({
+            duration: 40
+        }); 
+
+        setTimeout(() => {
+            navigate("/login")
+        }, 1500);
+    };
+
     return (
         <div className="center-container">
             <div id="home-container">
-                <p>Welcome</p>
+                <Link to="#" className="welcome-link" onClick={handleClick}>
+                    <p>Welcome</p>
+                </Link>
             </div>
       </div>
     );
